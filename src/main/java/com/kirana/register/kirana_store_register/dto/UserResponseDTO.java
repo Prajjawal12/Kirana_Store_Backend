ae78faba5e424478f07e44f9d10e05df5bb359e7
Prajjawal Deep Yadav <99403378+Prajjawal12@users.noreply.github.com>
2024-09-25 20:23:10 +0530

    Update UserResponseDTO.java

--- src/main/java/com/kirana/register/kirana_store_register/dto/UserResponseDTO.java
+++ src/main/java/com/kirana/register/kirana_store_register/dto/UserResponseDTO.java
@@ -7,6 +7,6 @@ import lombok.Data;
  */
 @Data
 public class UserResponseDTO {
-  private String token; // Authentication token for the user
-  private String role; // Role of the user (e.g., admin, user)
+  private String token; 
+  private String role;
 }
