8705e84b38f68469af8a0a645329af64643e2f11
Prajjawal Deep Yadav <99403378+Prajjawal12@users.noreply.github.com>
2024-09-25 20:22:39 +0530

    Update ReportResponseDTO.java

--- src/main/java/com/kirana/register/kirana_store_register/dto/ReportResponseDTO.java
+++ src/main/java/com/kirana/register/kirana_store_register/dto/ReportResponseDTO.java
@@ -7,9 +7,9 @@ import lombok.Data;
  */
 @Data
 public class ReportResponseDTO {
-  private String reportType; // Type of report
-  private double totalCredits; // Total credits in the report
-  private double totalDebits; // Total debits in the report
-  private double netFlow; // Net flow of the report
-  private String currency; // Currency used in the report
+  private String reportType; 
+  private double totalCredits; 
+  private double totalDebits;
+  private double netFlow;
+  private String currency; 
 }
